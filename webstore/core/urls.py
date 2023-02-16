25b443782dce3c36c57826b8a9dc8ad7584fb0ca
igornieb <igornieb@gmail.com>
2023-02-16 12:29:12 +0100

    basic product views

--- webstore/core/urls.py
+++ webstore/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import *
+from . import views
 
 urlpatterns = [
-
+    path('', views.ProductList.as_view(), name='product_list'),
+    path('product/<slug>', views.ProductDetail.as_view(), name='product'),
 ]
--- webstore/core/views.py
+++ webstore/core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class ProductList(ListView):
+    model = Product
+    template_name = 'home.html'
+    ordering = '-no_of_items_sold'
+    paginate_by = 60
+
+
+class ProductDetail(DetailView):
+    def get(self, request, slug):
+        product = Product.objects.get(slug=slug)
+        context = {'product':product,}
+        return render(request, 'product_detail.html', context)
+
+
+
+
