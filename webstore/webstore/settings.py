a0a073b7b9f4ad3669c1c55fb285ac03d6238af3
igornieb <igornieb@gmail.com>
2023-02-16 13:24:32 +0100

    added templates directory

--- webstore/webstore/settings.py
+++ webstore/webstore/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'webstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
