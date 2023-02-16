0a5d6875115b2a592d4d462d3c06ece6a7b12731
igornieb <igornieb@gmail.com>
2023-02-16 13:40:52 +0100

    added sorting to paginated ProductList

--- webstore/core/views.py
+++ webstore/core/views.py
@@ -5,17 +5,30 @@ from django.views.generic import ListView, DetailView
 
 class ProductList(ListView):
     model = Product
-    template_name = 'home.html'
-    ordering = '-no_of_items_sold'
-    paginate_by = 60
+    template_name = 'test.html'
+    paginate_by = 1
+
+    def get_queryset(self):
+        if self.request.GET.get("order_by"):
+            order = {
+                'bestselers': '-no_of_items_sold',
+                'price_a': 'current_price',
+                'price_d': '-current_price',
+            }
+            order_by = order[self.request.GET.get("order_by")]
+            queryset = Product.objects.all().order_by(order_by)
+        else:
+            queryset = Product.objects.all().order_by('-no_of_items_sold')
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductList, self).get_context_data(**kwargs)
+        context['input'] = self.request.GET.get("order_by")
+        return context
 
 
 class ProductDetail(DetailView):
     def get(self, request, slug):
         product = Product.objects.get(slug=slug)
-        context = {'product':product,}
+        context = {'product': product, }
         return render(request, 'product_detail.html', context)
-
-
-
-
