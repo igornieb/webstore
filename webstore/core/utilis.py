d084254a6a290fb3e14b28385ed6e407b270e33a
igornieb <igornieb@gmail.com>
2023-02-17 12:13:00 +0100

    added function that sums carts total value

--- webstore/core/utilis.py
+++ webstore/core/utilis.py
@@ -1,5 +1,14 @@
+from django.contrib.sessions.models import Session
+
 product_order = {
-                'bestselers': '-no_of_items_sold',
-                'price_a': 'current_price',
-                'price_d': '-current_price',
-            }
+    'bestselers': '-no_of_items_sold',
+    'price_a': 'current_price',
+    'price_d': '-current_price',
+}
+
+
+def total_amount_for_session(queryset):
+    total = 0
+    for cart in queryset:
+        total += cart.total()
+    return total
