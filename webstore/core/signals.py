7fabb8566b13f539af83c869a926bc37d722f64b
igornieb <igornieb@gmail.com>
2023-02-28 23:10:16 +0100

    create customeraddress on account create

--- webstore/core/signals.py
+++ webstore/core/signals.py
@@ -1,11 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import User, Customer
+from .models import User, Customer, CustomerAddress
 
 
 def create_customer(sender, instance, created, **kwargs):
     if created:
-        Customer.objects.create(user=instance)
+        customer = Customer.objects.create(user=instance)
+        CustomerAddress.objects.create(customer=customer)
 
 
 post_save.connect(create_customer, sender=User)
