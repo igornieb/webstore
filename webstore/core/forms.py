b0f09f9ea5dd12c8b8d257209c44981ffa8b1036
igornieb <igornieb@gmail.com>
2023-02-28 23:10:24 +0100

    checkout forms

--- webstore/core/forms.py
+++ webstore/core/forms.py
@@ -1,6 +1,17 @@
 from django import forms
+from .models import CustomerAddress
 
 
 class CartForm(forms.Form):
     slug = forms.SlugField()
     quantity = forms.IntegerField(min_value=0)
+
+
+class DiscountForm(forms.Form):
+    name = forms.CharField(max_length=20)
+
+
+class CustomerAddressForm(forms.ModelForm):
+    class Meta:
+        model = CustomerAddress
+        fields = '__all__'
