4111342d15a392e56b960748595505e43cfc568c
igornieb <igornieb@gmail.com>
2023-03-01 21:34:50 +0100

    added login url and redirect_url

--- webstore/webstore/settings.py
+++ webstore/webstore/settings.py
@@ -128,3 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, '/static/media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'product_list'
+LOGIN_URL = 'login'
