d3a5eef42bd92e1e63ae8661d4d55e97d0c379a8
igornieb <igornieb@gmail.com>
2023-03-02 16:53:35 +0100

    added product serializer

--- webstore/api/serializers.py
+++ webstore/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import Customer, CustomerAddress
+from core.models import Customer, CustomerAddress, Product
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -16,3 +16,12 @@ class CustomerAddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomerAddress
         fields = ['customer', 'firstname', 'lastname', 'city', 'postcode', 'street', 'house_number']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='product-details', read_only=True, lookup_field='slug')
+    brand = serializers.CharField(source="brand.name")
+    category = serializers.CharField(source="category.name")
+    class Meta:
+        model = Product
+        fields = ('url', 'name','category','brand','image','current_price','base_price', 'description', 'no_of_items_in_stock')
