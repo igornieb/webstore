7c7cbbd11e9fe160194c208f8ab88f925b30ccbb
igornieb <igornieb@gmail.com>
2023-03-03 14:42:10 +0100

    added register view

--- webstore/api/urls.py
+++ webstore/api/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', CreateUserView.as_view()),
-
 ]
