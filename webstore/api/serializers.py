00f7ce21724a1b7ea5d068187102666fdf11684d
igornieb <igornieb@gmail.com>
2023-03-03 14:41:46 +0100

    added register serializer

--- webstore/api/serializers.py
+++ webstore/api/serializers.py
@@ -41,3 +41,21 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = ('item', 'quantity', 'total')
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+
+        return user
+
+    class Meta:
+        model = User
+        fields = ("username", "password", )
