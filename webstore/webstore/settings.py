9923d27a094e8bc9332b4ef4d40c37edafe3949f
igornieb <igornieb@gmail.com>
2023-03-08 19:42:37 +0100

    added redis settings

--- webstore/webstore/settings.py
+++ webstore/webstore/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", 'not so secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEBUG", 0)))
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'phonenumber_field',
+    'django_celery_beat',
     'rest_framework',
     'api',
     'core',
@@ -143,3 +144,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
+
+# celery
+CELERY_BROKER_URL = "redis://redis-webstore:6379"
+CELERY_RESULT_BACKEND = "redis://redis-webstore:6379"
